d269909f6e103d61300a56a587eed6d63d215392
Joshhughes34 <joshuahughes34@msn.com>
2016-02-12 19:09:26 +0000

    Start Up tweaking + http request
    first volley http request to get crime locations
    changed start up activity to reflect a good splash screen

--- FypApp/app/src/main/java/com/example/joshuahughes/fypapp/DashBoardActivity.java
+++ FypApp/app/src/main/java/com/example/joshuahughes/fypapp/DashBoardActivity.java
@@ -1,11 +1,17 @@
 package com.example.joshuahughes.fypapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.DialogPreference;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class DashBoardActivity extends AppCompatActivity {
 
@@ -16,6 +22,38 @@ public class DashBoardActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Intent intent = getIntent();
+
+        String jsonString = intent.getStringExtra("crimeLocationTypes");
+        TextView textview = (TextView)findViewById(R.id.textView2);
+        textview.setText(jsonString);
+
+        Boolean isConnected = intent.getExtras().getBoolean("isConnected");
+
+        if(!isConnected){
+            TextView textview0 =  (TextView)findViewById(R.id.textView);
+            textview0.setText("OFFLINE MODE");
+            createOfflineModeDialog();
+        }
+    }
+
+    private void createOfflineModeDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(R.string.loadingProgress_NoInternetMessage);
+        builder.setCancelable(false);
+        builder.setPositiveButton(
+                "Continue",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                }
+        );
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
 }
--- FypApp/app/src/main/java/com/example/joshuahughes/fypapp/StartUpActivity.java
+++ FypApp/app/src/main/java/com/example/joshuahughes/fypapp/StartUpActivity.java
@@ -1,75 +1,82 @@
 package com.example.joshuahughes.fypapp;
 
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-
-import java.io.InputStream;
-import java.net.URL;
+import android.util.Log;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import org.json.JSONArray;
 
 public class StartUpActivity extends AppCompatActivity {
 
     protected Intent intent;
+    protected boolean isConnected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_up);
 
-        TextView loadingText = (TextView) findViewById(R.id.loadingProgressText);
-
-        loadingText.setText(R.string.loadingProgress_InternetConnection);
-        boolean isConnected = isConnected();
+        isConnected = isConnected();
 
         intent = new Intent(this, DashBoardActivity.class);
 
-        if(isConnected) {
-            startActivity(intent);
+        if (isConnected) {
+            VolleyTest();
+        } else {
+            FinishStartup(null);
         }
-        else {
-            createAlertDialog();
-        }
-
-        finish(); //finish so this cant be returned to
-    }
-
-    private void createAlertDialog(){
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder.setMessage(R.string.loadingProgress_NoInternetMessage);
-
-        builder.setCancelable(false);
-        builder.setPositiveButton(
-                "Continue",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                        startActivity(intent);
-                    }
-                }
-        );
-
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
     }
 
 
-    private boolean isConnected(){
+    private boolean isConnected() {
 
-        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = cm.getActiveNetworkInfo();
         return (networkInfo != null && networkInfo.isConnectedOrConnecting());
 
     }
 
+    private void VolleyTest() {
+        //RequestQueue queue = Volley.newRequestQueue(this);
+
+        final String url = "http://co-web-1.lboro.ac.uk/cojh5web/api/crimelocation";
+
+        JsonArrayRequest getRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("Response", response.toString());
+
+                        FinishStartup(response);
+                    }
+                }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("Error.Response", error.toString());
+                }
+            }
+        );
+
+        //queue.add(getRequest);
+        MySingleton.getInstance(this).addToRequestQueue(getRequest);
+    }
+
+
+    private void FinishStartup(JSONArray jsonArray) {
+        if(jsonArray != null){
+            intent.putExtra("crimeLocationTypes", jsonArray.toString());
+        }
+        intent.putExtra("isConnected", isConnected);
+        startActivity(intent);
+
+        finish();
+    }
 
 }
