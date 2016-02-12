a4680a1790ec6ed344709a027c57a42f42ee8c01
Joshhughes34 <joshuahughes34@msn.com>
2016-02-12 15:04:25 +0000

    Start Up activity creation
    created basic startup activity and intent

--- FypApp/app/src/main/java/com/example/joshuahughes/fypapp/StartUpActivity.java
+++ FypApp/app/src/main/java/com/example/joshuahughes/fypapp/StartUpActivity.java
@@ -1,13 +1,70 @@
 package com.example.joshuahughes.fypapp;
 
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StartUpActivity extends AppCompatActivity {
+
+    protected Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up);
+
+        TextView loadingText = (TextView) findViewById(R.id.loadingProgressText);
+
+        loadingText.setText(R.string.loadingProgress_InternetConnection);
+        boolean isConnected = isConnected();
+
+        intent = new Intent(this, DashBoardActivity.class);
+
+        if(isConnected) {
+            startActivity(intent);
+        }
+        else {
+            createAlertDialog();
+        }
+
+        finish(); //finish so this cant be returned to
+    }
+
+    private void createAlertDialog(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(R.string.loadingProgress_NoInternetMessage);
+
+        builder.setCancelable(false);
+        builder.setPositiveButton(
+                "Continue",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
+    private boolean isConnected(){
+
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnectedOrConnecting());
+
     }
 }
