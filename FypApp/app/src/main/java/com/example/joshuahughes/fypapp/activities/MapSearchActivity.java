8cacbfbf311edf4013625130b7afa50be6c1faa1
Joshhughes34 <joshuahughes34@msn.com>
2016-02-26 10:39:25 +0000

    26/02/2016 10:38PM
    added Google play services dependencies and tested creating a map

--- FypApp/app/src/main/java/com/example/joshuahughes/fypapp/activities/MapSearchActivity.java
+++ FypApp/app/src/main/java/com/example/joshuahughes/fypapp/activities/MapSearchActivity.java
@@ -1,32 +1,54 @@
 package com.example.joshuahughes.fypapp.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.joshuahughes.fypapp.R;
 import com.example.joshuahughes.fypapp.VolleyQueue;
-import com.example.joshuahughes.fypapp.helpers.StorageHelper;
+import com.example.joshuahughes.fypapp.helpers.PermissionUtils;
 import com.example.joshuahughes.fypapp.models.CrimeLocationTypeModel;
-import com.google.gson.JsonObject;
 
-import org.json.JSONArray;
+import com.google.android.gms.maps.CameraUpdateFactory;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
 import org.json.JSONObject;
 
-public class MapSearchActivity extends BaseActivity {
+
+public class MapSearchActivity extends BaseActivity implements GoogleMap.OnMyLocationButtonClickListener, OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private GoogleMap mMap;
+    /**
+     * Request code for location permission request.
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mPermissionDenied = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,14 +84,17 @@ public class MapSearchActivity extends BaseActivity {
         textview.setText(testUrl);
 
 
-
         Button testButton = (Button) findViewById(R.id.button);
-        testButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        testButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 progressBar.setVisibility(View.VISIBLE);
                 GetCrimeLocationJson(testUrl, progressBar);
             }
         });
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
     }
 
@@ -97,5 +122,72 @@ public class MapSearchActivity extends BaseActivity {
         );
 
         VolleyQueue.getInstance(this).addToRequestQueue(getRequest);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+        mMap = map;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(52.769261, -1.2272118), 14.0f));
+        mMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 }
