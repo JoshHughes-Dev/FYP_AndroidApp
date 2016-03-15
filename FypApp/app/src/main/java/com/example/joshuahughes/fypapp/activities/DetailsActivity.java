9a0f9400533e39cae92c7a804367fca333728739
Joshhughes34 <joshuahughes34@msn.com>
2016-03-15 19:41:02 +0000

    15/03/2016 7:42PM
    added graph package
    added graph view to crime details page

--- FypApp/app/src/main/java/com/example/joshuahughes/fypapp/activities/DetailsActivity.java
+++ FypApp/app/src/main/java/com/example/joshuahughes/fypapp/activities/DetailsActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -13,9 +15,13 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.example.joshuahughes.fypapp.R;
+import com.example.joshuahughes.fypapp.YearMonthComparator;
 import com.example.joshuahughes.fypapp.adapters.CrimesAdapter;
+import com.example.joshuahughes.fypapp.fragments.CrimeGraphFragment;
+import com.example.joshuahughes.fypapp.fragments.CrimesListFragment;
 import com.example.joshuahughes.fypapp.helpers.myHelper;
 import com.example.joshuahughes.fypapp.models.CrimeLocationModel;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -26,17 +32,21 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
-public class DetailsActivity extends BaseActivity implements OnMapReadyCallback {
+public class DetailsActivity extends BaseActivity implements OnMapReadyCallback, CrimesListFragment.OnFragmentInteractionListener, CrimeGraphFragment.OnFragmentInteractionListener{
 
     private CrimeLocationModel crimeLocationModel;
     private GoogleMap mMap;
     private TableLayout detailsTable;
-    private ListView crimesListView;
-    private CrimesAdapter crimesAdapter;
-    private int numberOfLocationResults = 0;
 
+    private CrimesListFragment crimesListFragment;
+    private CrimeGraphFragment crimesGraphFragment;
+
+    //private ViewPager mViewPager;
+    private int numberOfLocationResults = 0;
+    private Boolean listOpen = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +70,10 @@ public class DetailsActivity extends BaseActivity implements OnMapReadyCallback 
 
         setTitle(crimeLocationModel.Location.Name);
 
+
+        //sort crimeLocationModel
+        Collections.sort(crimeLocationModel.Crimes, new YearMonthComparator());
+
         //Init map fragment
         MapFragment liteMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.details_lite_map);
         liteMapFragment.getMapAsync(this);
@@ -71,8 +85,27 @@ public class DetailsActivity extends BaseActivity implements OnMapReadyCallback 
         createBadgeDetailTableRow();
         createDistanceDetailTableRow();
 
-        //create crimes list
-        CreateCrimesListView();
+        //create crimes list fragment
+        crimesListFragment = (CrimesListFragment) getSupportFragmentManager().findFragmentById(R.id.crimes_list_fragment);
+        crimesGraphFragment = (CrimeGraphFragment) getSupportFragmentManager().findFragmentById(R.id.crimes_graph_fragment);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if(listOpen){
+            ft.hide(crimesGraphFragment);
+        }
+        else{
+            ft.hide(crimesListFragment);
+        }
+        ft.commit();
+
+
+        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.listGraph_toggleButton);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                temp();
+            }
+        });
     }
 
     @Override
@@ -88,25 +121,35 @@ public class DetailsActivity extends BaseActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        mMap = googleMap;
-
         LatLng locationCoords = new LatLng(crimeLocationModel.Location.Latitude, crimeLocationModel.Location.Longitude);
-
+        mMap = googleMap;
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(locationCoords, 16));
-
         mMap.addMarker(new MarkerOptions().position(locationCoords));
-
     }
 
 
-    protected void CreateCrimesListView(){
 
-        crimesListView = (ListView)findViewById(R.id.crimes_listView);
-        crimesAdapter = new CrimesAdapter(this, crimeLocationModel.Crimes);
-        Log.d("DetailsActivity", Integer.toString(crimeLocationModel.Crimes.size()));
-        crimesListView.setAdapter(crimesAdapter);
+    @Override
+    public void onListLoadSavedInstance(){
 
+        if(crimeLocationModel != null){
+            if(crimesListFragment != null){
+                crimesListFragment.CreateCrimesListView(crimeLocationModel);
+            }
+        }
     }
+
+    @Override
+    public void onGraphLoadSavedInstance(){
+
+        if(crimeLocationModel != null){
+            if(crimesListFragment != null) {
+                crimesGraphFragment.CreateGraph(crimeLocationModel);
+            }
+        }
+    }
+
+
 
     private void createRankDetailTableRow(){
 
@@ -166,6 +209,27 @@ public class DetailsActivity extends BaseActivity implements OnMapReadyCallback 
         detailSubView.setText("...from starting search point");
 
         detailsTable.addView(tr);
+    }
+
+
+    private void temp(){
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        if(listOpen){
+            //change to graph
+            ft.show(crimesGraphFragment);
+            ft.hide(crimesListFragment);
+            listOpen = false;
+        }
+        else{
+            //change to map view
+            ft.hide(crimesGraphFragment);
+            ft.show(crimesListFragment);
+            listOpen = true;
+        }
+
+        ft.commit();
     }
 
 }
---
+++ FypApp/app/src/main/java/com/example/joshuahughes/fypapp/fragments/CrimeGraphFragment.java
@@ -0,0 +1,207 @@
+package com.example.joshuahughes.fypapp.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.joshuahughes.fypapp.R;
+import com.example.joshuahughes.fypapp.models.CrimeLocationModel;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.text.DateFormatSymbols;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link CrimeGraphFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link CrimeGraphFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CrimeGraphFragment extends Fragment {
+
+
+    private CrimeLocationModel crimeLocationModel;
+    private GraphView graphView;
+
+    private OnFragmentInteractionListener mListener;
+
+    public CrimeGraphFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+
+     * @return A new instance of fragment CrimeGraphFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CrimeGraphFragment newInstance() {
+        CrimeGraphFragment fragment = new CrimeGraphFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) { }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_crime_graph, container, false);
+
+        graphView = (GraphView) v.findViewById(R.id.crimes_graphView);
+        return v;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    //have to wait until activity is create before getting results model from parent activity
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        mListener.onGraphLoadSavedInstance();
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onGraphLoadSavedInstance();
+    }
+
+    public void CreateGraph(CrimeLocationModel model){
+        crimeLocationModel = model;
+
+        ArrayList<Integer> timePeriodList = GetTimePeriodIntList();
+        String[] monthStr = ConvertIntToStringMonths(timePeriodList);
+
+        setGraphSettings(monthStr);
+
+        ArrayList<DataPoint> dataPoints = GetDataPointsFromCrimeLocationModel(timePeriodList);
+
+        DataPoint[] dataPointsArray = new DataPoint[dataPoints.size()];
+        dataPointsArray = dataPoints.toArray(dataPointsArray);
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPointsArray);
+
+        graphView.addSeries(series);
+
+    }
+
+    private ArrayList<DataPoint> GetDataPointsFromCrimeLocationModel(ArrayList<Integer> timePeriodList){
+        ArrayList<DataPoint> dataPoints = new ArrayList<DataPoint>();
+
+        //iterate through each timeperiod month
+        for(int i = 0; i<timePeriodList.size(); i++){
+
+            //count crime occurences
+            int countOccurences = 0;
+            for(int j = 0; j< crimeLocationModel.Crimes.size(); j++){
+                if(crimeLocationModel.Crimes.get(j).Month == timePeriodList.get(i)){
+                    countOccurences++;
+                }
+            }
+
+            //add new data point at index i
+            dataPoints.add(new DataPoint(i, countOccurences));
+
+        }
+
+        return dataPoints;
+    }
+
+    private void setGraphSettings(String[] labels){
+
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graphView);
+        staticLabelsFormatter.setHorizontalLabels(labels);
+
+        graphView.getGridLabelRenderer().setNumHorizontalLabels(12);
+        graphView.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+        graphView.setScaleY(1);
+        graphView.setScaleX(1);
+
+    }
+
+    private ArrayList<Integer> GetTimePeriodIntList(){
+
+        ArrayList<Integer> timePeriodList = new ArrayList<>();
+
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int currentMonth = Calendar.getInstance().get(Calendar.MONTH);
+
+        int previousYear = currentYear - 1;
+
+        for(int i = currentMonth; i<13; i++)
+        {
+            timePeriodList.add(i);
+        }
+        for(int j = 1; j<currentMonth; j++)
+        {
+            timePeriodList.add(j);
+        }
+
+        return timePeriodList;
+    }
+
+    private String[] ConvertIntToStringMonths(ArrayList<Integer> timePeriodList){
+
+        ArrayList<String> monthStrs = new ArrayList<String>();
+
+        for(int i=0; i< timePeriodList.size(); i++){
+            String monthLong = new DateFormatSymbols().getMonths()[timePeriodList.get(i)-1];
+
+            monthStrs.add(monthLong.substring(0, Math.min(monthLong.length(), 3)));
+        }
+
+        String [] monthStrsArray = new String[monthStrs.size()];
+        monthStrsArray = monthStrs.toArray(monthStrsArray);
+
+        return monthStrsArray;
+    }
+}
+
+
